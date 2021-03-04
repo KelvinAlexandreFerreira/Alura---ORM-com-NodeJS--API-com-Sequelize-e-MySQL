a19656c1318bd0bac3a4e149f4ca215eb118c02b
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-03-04 07:21:03 -0400

    Populando o banco

---
+++ orm-sequelize/api/seeders/20210304110651-demo-pessoa.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Pessoas', [{
+      nome: 'Ana Souza',
+      ativo: true,
+      email: 'ana@ana.com',
+      role: 'estudante',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Pessoas', null, {});
+  }
+};
