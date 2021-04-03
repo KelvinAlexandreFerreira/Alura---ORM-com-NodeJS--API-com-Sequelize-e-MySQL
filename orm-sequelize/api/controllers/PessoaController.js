7c3b57215dca1f4b9d8a723b7f9e02240bdcddff
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-04-03 07:12:16 -0400

    Lendo uma matrícula

--- orm-sequelize/api/controllers/PessoaController.js
+++ orm-sequelize/api/controllers/PessoaController.js
@@ -55,6 +55,24 @@ class PessoaController {
             return res.status(500).json(error.message) 
         }
     }
+
+    //http://localhost:3000/pessoas/1/matriculas/5
+    //http://localhost:3000/pessoas/:estudanteId/matriculas/:matriculaId
+
+    static async pegaUmaMatricula(req,res){
+        const { estudanteId, matriculaId } = req.params
+        try {
+            const umaMatricula = await database.Matriculas.findOne({
+                where: {
+                    id: Number(matriculaId),
+                    estudante_id: Number(estudanteId)
+                }
+            })
+            return res.status(200).json(umaMatricula)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = PessoaController
