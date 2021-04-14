d91d32959399abaca95aedc810247a54879931e8
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-04-14 06:45:45 -0400

    Outros escopos

--- orm-sequelize/api/models/pessoas.js
+++ orm-sequelize/api/models/pessoas.js
@@ -29,6 +29,10 @@ module.exports = (sequelize, DataTypes) => {
     paranoid: true,
     defaultScope: {
       where: { ativo: true }
+    },
+    scopes: {
+      todos: { where: {} },
+      //etc: { constraint: valor }
     }
   });
   return Pessoas;
