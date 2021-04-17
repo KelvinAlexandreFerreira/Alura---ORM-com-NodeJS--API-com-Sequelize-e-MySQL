3f74e69056fd05c7562f560d26d5e44b821163e4
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-04-17 09:08:14 -0400

    Usando mixins

--- orm-sequelize/api/routes/pessoasRoute.js
+++ orm-sequelize/api/routes/pessoasRoute.js
@@ -6,14 +6,19 @@ const router = Router()
 router.get('/pessoas', PessoaController.pegaPessoasAtivas)
 router.get('/pessoas/todos', PessoaController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
-router.post('/pessoas', PessoaController.criaPessoa)
-router.put('/pessoas/:id', PessoaController.atualizaPessoa)
-router.delete('/pessoas/:id', PessoaController.apagaPessoa)
 router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
-router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
-router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
-router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
+router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
+
+router.post('/pessoas', PessoaController.criaPessoa)
 router.post('/pessoas/:id/restaura', PessoaController.restauraPessoa)
 router.post('/pessoas/:estudanteId/matricula/:matriculaId/restaura', PessoaController.restauraMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+
 
 module.exports = router
