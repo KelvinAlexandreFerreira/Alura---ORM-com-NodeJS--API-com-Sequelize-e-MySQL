ff15f956c9f36eb1ca0b41cdefda8e0be83ea3c3
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-04-26 20:59:35 -0400

    Adicionando serviços

---
+++ orm-sequelize/api/services/PessoasServices.js
@@ -0,0 +1,10 @@
+const Services = require('./Services')
+
+class PessoasServices extends Services{
+    constructor(){
+        super('Pessoas')
+    }
+    // Métodos ospecíficos do controlador de Pessoas
+}
+
+module.exports = PessoasServices
