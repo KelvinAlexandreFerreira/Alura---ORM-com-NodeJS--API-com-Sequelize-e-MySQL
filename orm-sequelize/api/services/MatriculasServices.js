1f7781dc9e8bb0014b3c2075d3b6f3e3db6bcfab
Kelvin Alexandre Ferreira <alexandre_kelvin@hotmail.com>
2021-04-28 07:09:25 -0400

    Passando parâmetros

---
+++ orm-sequelize/api/services/MatriculasServices.js
@@ -0,0 +1,10 @@
+const Services = require('./Services')
+
+class MatriculasServices extends Services{
+    constructor(){
+        super('Matriculas')
+    }
+    // Métodos ospecíficos do controlador de Matriculas
+}
+
+module.exports = MatriculasServices
